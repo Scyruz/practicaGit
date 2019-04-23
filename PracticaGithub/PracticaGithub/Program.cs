56b496a1e95ed51882f7acfc737ba371f8ed467a
Priscila Ruiz Gámez <A01561869@itesm.mx>
2019-04-22 21:55:53 -0600

    commit segundo

--- PracticaGithub/PracticaGithub/Program.cs
+++ PracticaGithub/PracticaGithub/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace PracticaGithub
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+            if (File.Exists("numeros.txt"))
+            {
+                // Console.WriteLine("Todo bien");
+                string[] text = File.ReadAllLines("numeros.txt");
+                int numLineas = text.Length;
+                
+                for (int i = 0; i < numLineas; i++)
+                {
+                    string linea = text[i];
+                    string[] nums = linea.Split();
+                    int numNums = nums.Length;
+
+                    for (int u = 0; u < numNums; u++)
+                    {
+                        string numeroString = nums[u];
+                        int numInt = Int32.Parse(numeroString);
+                        if (numInt % 10 == 0)
+                        {
+                            counter++;
+                        }
+
+                    }
+                }
+                Console.WriteLine(counter);
+            }
+            else
+            {
+                Console.WriteLine("Todo mal");
+            }
+        
         }
     }
 }
